4ab19651ad36ec1d94ff2d591fd9ab6f56c10328
Alejandro Encarnación <alejandroencarnacion1@gmail.com>
2024-02-05 11:20:30 -0400

    Add APIError interface to handle errors through an interface

---
+++ internal/api/error.go
@@ -0,0 +1,84 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// ErrorCode error code for each type of error in the service
+type ErrorCode string
+
+var (
+	// ErrCodeInternalServerError error code when service suffered an unexpected error
+	ErrCodeInternalServerError ErrorCode = "internal_server_error"
+	// ErrCodeInvalidRequestError error code when service received an invalid request
+	ErrCodeInvalidRequestError ErrorCode = "invalid_request"
+	// ErrCodeResourceNotFound error code when resource was not found
+	ErrCodeResourceNotFound ErrorCode = "resource_not_found"
+)
+
+// APIError interface to handle API errors in the service
+type APIError interface {
+	error
+	Unwrap() error
+	HTTPStatusCode() int
+	Code() ErrorCode
+}
+
+// APIErr error type to standardize errors in the service
+type APIErr struct {
+	ErrCode    ErrorCode `json:"code"`
+	StatusCode int       `json:"status_code"`
+	Message    string    `json:"message"`
+	err        error
+}
+
+// Unwrap returns the error
+func (e APIErr) Unwrap() error {
+	return e.err
+}
+
+// HTTPStatusCode return HTTP status code assigned to the error
+func (e APIErr) HTTPStatusCode() int {
+	return e.StatusCode
+}
+
+// Code returns the error code assigned to the error
+func (e APIErr) Code() ErrorCode {
+	return e.ErrCode
+}
+
+// Error returns a formatted error message
+func (e APIErr) Error() string {
+	return fmt.Sprintf("(%d) %s", e.StatusCode, e.Message)
+}
+
+// NewInternalServerError API error to handle unexpected behavior in the service
+func NewInternalServerError(err error) APIErr {
+	return APIErr{
+		ErrCode:    ErrCodeInternalServerError,
+		StatusCode: http.StatusInternalServerError,
+		Message:    "Internal server error, please try again later",
+		err:        err,
+	}
+}
+
+// NewInvalidRequestError API error to handle invalid request in the service
+func NewInvalidRequestError(err error) APIErr {
+	return APIErr{
+		ErrCode:    ErrCodeInvalidRequestError,
+		StatusCode: http.StatusBadRequest,
+		Message:    fmt.Sprintf("Invalid request: %s", err.Error()),
+		err:        err,
+	}
+}
+
+// NewResourceNotFoundError API error when resource was not found in the service
+func NewResourceNotFoundError(err error, resource string) APIErr {
+	return APIErr{
+		ErrCode:    ErrCodeResourceNotFound,
+		StatusCode: http.StatusNotFound,
+		Message:    fmt.Sprintf("Resource %s not found", resource),
+		err:        err,
+	}
+}
---
+++ internal/api/error_test.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAPIErr(t *testing.T) {
+	c := require.New(t)
+
+	customErr := errors.New("custom error")
+
+	testCases := []struct {
+		runFunc    func(err error, resource string) error
+		errCode    ErrorCode
+		statusCode int
+		ErrMessage string
+		resource   string
+		err        error
+	}{
+		{
+			runFunc: func(err error, resource string) error {
+				return NewInternalServerError(err)
+			},
+			errCode:    ErrCodeInternalServerError,
+			statusCode: http.StatusInternalServerError,
+			ErrMessage: "(500) Internal server error, please try again later",
+			resource:   "",
+			err:        customErr,
+		},
+		{
+			runFunc: func(err error, resource string) error {
+				return NewInvalidRequestError(err)
+			},
+			errCode:    ErrCodeInvalidRequestError,
+			statusCode: http.StatusBadRequest,
+			ErrMessage: fmt.Sprintf("(400) Invalid request: %s", customErr.Error()),
+			resource:   "",
+			err:        customErr,
+		},
+		{
+			runFunc: func(err error, resource string) error {
+				return NewResourceNotFoundError(err, resource)
+			},
+			errCode:    ErrCodeResourceNotFound,
+			statusCode: http.StatusNotFound,
+			ErrMessage: "(404) Resource transaction not found",
+			resource:   "transaction",
+			err:        customErr,
+		},
+	}
+
+	for _, testCase := range testCases {
+		var apiErr APIError
+
+		err := testCase.runFunc(testCase.err, testCase.resource)
+
+		c.ErrorAs(err, &apiErr)
+		c.ErrorIs(err, apiErr)
+		c.Equal(testCase.errCode, apiErr.Code())
+		c.Equal(testCase.statusCode, apiErr.HTTPStatusCode())
+		c.EqualError(apiErr, testCase.ErrMessage)
+	}
+}
