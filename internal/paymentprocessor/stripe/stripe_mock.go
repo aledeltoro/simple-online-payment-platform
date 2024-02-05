93158812f411157e2c11f20a0c2dbd525d9d7c24
Alejandro Encarnación <alejandroencarnacion1@gmail.com>
2024-02-05 00:59:45 -0400

    Add mockStripeBackend struct to implement Backend interface from stripe library, in order to mock API calls

--- internal/paymentprocessor/stripe/stripe_mock.go
+++ internal/paymentprocessor/stripe/stripe_mock.go
@@ -1,8 +1,12 @@
 package stripe
 
 import (
+	"bytes"
+
 	"github.com/aledeltoro/simple-online-payment-platform/internal/models"
 	"github.com/stretchr/testify/mock"
+	"github.com/stripe/stripe-go/v76"
+	"github.com/stripe/stripe-go/v76/form"
 )
 
 // MockStripe mock object for Stripe implementation
@@ -33,4 +37,42 @@ func (m *MockStripe) RefundTransaction(metadata map[string]interface{}) (*models
 	args := m.Called(metadata)
 
 	return args.Get(0).(*models.Transaction), args.Error(1)
+}
+
+// mockStripeBackend mock for Stripe Backend interface
+type mockStripeBackend struct {
+	mock.Mock
+}
+
+// Call mock for Call method in Stripe Backend interface
+func (m *mockStripeBackend) Call(method, path, key string, params stripe.ParamsContainer, v stripe.LastResponseSetter) error {
+	args := m.Called(method, path, key, params, v)
+
+	return args.Error(0)
+}
+
+// CallStreaming mock for Call method in Stripe Backend interface
+func (m *mockStripeBackend) CallStreaming(method, path, key string, params stripe.ParamsContainer, v stripe.StreamingLastResponseSetter) error {
+	args := m.Called(method, path, key, params, v)
+
+	return args.Error(0)
+}
+
+// CallRaw mock for Call method in Stripe Backend interface
+func (m *mockStripeBackend) CallRaw(method, path, key string, body *form.Values, params *stripe.Params, v stripe.LastResponseSetter) error {
+	args := m.Called(method, path, key, params, v)
+
+	return args.Error(0)
+}
+
+// CallMultipart mock for Call method in Stripe Backend interface
+func (m *mockStripeBackend) CallMultipart(method, path, key, boundary string, body *bytes.Buffer, params *stripe.Params, v stripe.LastResponseSetter) error {
+	args := m.Called(method, path, key, params, v)
+
+	return args.Error(0)
+}
+
+// CallMultipart mock for Call method in Stripe Backend interface
+func (m *mockStripeBackend) SetMaxNetworkRetries(maxNetworkRetries int64) {
+	m.Called(maxNetworkRetries)
 }
