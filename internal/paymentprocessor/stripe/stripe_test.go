443f04450076e6f5b2472da002e7486a3843a486
Alejandro Encarnación <alejandroencarnacion1@gmail.com>
2024-02-05 01:02:01 -0400

    Refactor tests from stripe package to use mockStripeBackend

--- internal/paymentprocessor/stripe/stripe_test.go
+++ internal/paymentprocessor/stripe/stripe_test.go
@@ -1,12 +1,13 @@
 package stripe
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/aledeltoro/simple-online-payment-platform/internal/models"
-	"github.com/oklog/ulid/v2"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"github.com/stripe/stripe-go/v76"
+	"github.com/stripe/stripe-go/v76/client"
 )
 
 func TestNew(t *testing.T) {
@@ -31,8 +32,66 @@ func TestPerformTransaction(t *testing.T) {
 	c := require.New(t)
 
 	expectedTransaction := &models.Transaction{
-		TransactionID: fmt.Sprintf("TXN_%s", ulid.Make().String()),
-		Status:        models.TransactionStatusSucceeded,
+		Status:      models.TransactionStatusPending,
+		Description: "Testing stripe service",
+		Provider:    models.PaymentProviderStripe,
+		Amount:      2000,
+		Currency:    "usd",
+		Type:        models.TransactionTypeCharge,
+		AdditionalFields: map[string]interface{}{
+			"charge_id":         "charge_id",
+			"payment_intent_id": "payment_intent_id",
+		},
+	}
+
+	expectedInput := &models.TransactionInput{
+		Amount:        2000,
+		Currency:      "usd",
+		PaymentMethod: "pm_card_visa",
+		Description:   "Testing stripe service",
+	}
+
+	stripeBackendMock := new(mockStripeBackend)
+	stripeTestBackends := &stripe.Backends{
+		API:     stripeBackendMock,
+		Connect: stripeBackendMock,
+		Uploads: stripeBackendMock,
+	}
+
+	stripeBackendMock.On("Call", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		mockPaymentIntentResult := args.Get(4).(*stripe.PaymentIntent)
+
+		*mockPaymentIntentResult = stripe.PaymentIntent{
+			ID:          "payment_intent_id",
+			Description: expectedTransaction.Description,
+			Amount:      int64(expectedTransaction.Amount),
+			Currency:    stripe.Currency(expectedTransaction.Currency),
+			LatestCharge: &stripe.Charge{
+				ID: "charge_id",
+			},
+		}
+	}).Return(nil)
+
+	mockStripeClient := client.New("sk_test", stripeTestBackends)
+
+	service := stripeService{
+		client: mockStripeClient,
+	}
+
+	transaction, err := service.PerformTransaction(expectedInput)
+	c.NoError(err)
+
+	transaction.TransactionID = ""
+
+	c.Equal(expectedTransaction, transaction)
+}
+
+func TestPerformTransactionCardError(t *testing.T) {
+	c := require.New(t)
+
+	expectedTransaction := &models.Transaction{
+		Status:        models.TransactionStatusFailure,
+		FailureReason: string(stripe.ErrorCodeCardDeclined),
 		Description:   "Testing stripe service",
 		Provider:      models.PaymentProviderStripe,
 		Amount:        2000,
@@ -51,32 +110,49 @@ func TestPerformTransaction(t *testing.T) {
 		Description:   "Testing stripe service",
 	}
 
-	mockService := MockStripe{}
+	stripeBackendMock := new(mockStripeBackend)
+	stripeTestBackends := &stripe.Backends{
+		API:     stripeBackendMock,
+		Connect: stripeBackendMock,
+		Uploads: stripeBackendMock,
+	}
 
-	mockService.On("PerformTransaction", expectedInput).Return(expectedTransaction, nil)
+	stripeErr := &stripe.Error{
+		Type: stripe.ErrorTypeCard,
+		Code: stripe.ErrorCodeCardDeclined,
+	}
 
-	transaction, err := mockService.PerformTransaction(expectedInput)
+	stripeBackendMock.On("Call", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		mockPaymentIntentResult := args.Get(4).(*stripe.PaymentIntent)
+
+		*mockPaymentIntentResult = stripe.PaymentIntent{
+			ID:          "payment_intent_id",
+			Description: expectedTransaction.Description,
+			Amount:      int64(expectedTransaction.Amount),
+			Currency:    stripe.Currency(expectedTransaction.Currency),
+			LatestCharge: &stripe.Charge{
+				ID: "charge_id",
+			},
+		}
+	}).Return(stripeErr)
+
+	mockStripeClient := client.New("sk_test", stripeTestBackends)
+
+	service := stripeService{
+		client: mockStripeClient,
+	}
+
+	transaction, err := service.PerformTransaction(expectedInput)
 	c.NoError(err)
-	c.Equal(expectedTransaction, transaction)
-}
-
-func TestQueryTransaction(t *testing.T) {
-	c := require.New(t)
-
-	mockService := MockStripe{}
-
-	mockService.On("QueryTransaction", "id").Return(nil, nil)
-
-	transaction, err := mockService.QueryTransaction("id")
-	c.Nil(transaction)
-	c.NoError(err)
+	c.Equal(expectedTransaction.Status, transaction.Status)
+	c.Equal(expectedTransaction.FailureReason, transaction.FailureReason)
 }
 
 func TestRefundTransaction(t *testing.T) {
 	c := require.New(t)
 
 	expectedTransaction := &models.Transaction{
-		Status: models.TransactionStatusSucceeded,
+		Status: models.TransactionStatusPending,
 		Type:   models.TransactionTypeRefund,
 		AdditionalFields: map[string]interface{}{
 			"charge_id":         "charge_id",
@@ -85,11 +161,81 @@ func TestRefundTransaction(t *testing.T) {
 		},
 	}
 
-	mockService := MockStripe{}
+	stripeBackendMock := new(mockStripeBackend)
+	stripeTestBackends := &stripe.Backends{
+		API:     stripeBackendMock,
+		Connect: stripeBackendMock,
+		Uploads: stripeBackendMock,
+	}
 
-	mockService.On("RefundTransaction", expectedTransaction.AdditionalFields).Return(expectedTransaction, nil)
+	stripeBackendMock.On("Call", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		mockRefund := args.Get(4).(*stripe.Refund)
 
-	updatedTransaction, err := mockService.RefundTransaction(expectedTransaction.AdditionalFields)
+		*mockRefund = stripe.Refund{
+			ID: "refund_id",
+			Charge: &stripe.Charge{
+				ID: "charge_id",
+			},
+			PaymentIntent: &stripe.PaymentIntent{
+				ID: "payment_intent_id",
+			},
+		}
+	}).Return(nil)
+
+	mockStripeClient := client.New("sk_test", stripeTestBackends)
+
+	service := stripeService{
+		client: mockStripeClient,
+	}
+
+	updatedTransaction, err := service.RefundTransaction(expectedTransaction.AdditionalFields)
 	c.NoError(err)
 	c.Equal(expectedTransaction, updatedTransaction)
+}
+
+func TestRefundTransactionAlreadyRefunded(t *testing.T) {
+	c := require.New(t)
+
+	expectedTransaction := &models.Transaction{
+		Status: models.TransactionStatusPending,
+		Type:   models.TransactionTypeRefund,
+		AdditionalFields: map[string]interface{}{
+			"charge_id":         "charge_id",
+			"payment_intent_id": "payment_intent_id",
+			"refund_id":         "refund_id",
+		},
+	}
+
+	stripeBackendMock := new(mockStripeBackend)
+	stripeTestBackends := &stripe.Backends{
+		API:     stripeBackendMock,
+		Connect: stripeBackendMock,
+		Uploads: stripeBackendMock,
+	}
+
+	stripeErr := &stripe.Error{
+		Type: stripe.ErrorTypeInvalidRequest,
+		Code: stripe.ErrorCodeChargeAlreadyRefunded,
+	}
+
+	stripeBackendMock.On("Call", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(stripeErr)
+
+	mockStripeClient := client.New("sk_test", stripeTestBackends)
+
+	service := stripeService{
+		client: mockStripeClient,
+	}
+
+	updatedTransaction, err := service.RefundTransaction(expectedTransaction.AdditionalFields)
+	c.Nil(updatedTransaction)
+	c.ErrorIs(err, ErrChargeAlreadyRefunded)
+}
+
+func TestRefundTransactionMissingChargeID(t *testing.T) {
+	c := require.New(t)
+
+	service := stripeService{}
+
+	_, err := service.RefundTransaction(map[string]interface{}{})
+	c.ErrorIs(err, ErrMissingChargeID)
 }
